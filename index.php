78841a3548bf6a2743530d47f75feafb451bcbde
azankhokhar47 <azankhokhar203@gmail.com>
2025-10-23 11:48:06 +0500

    ...

---
+++ index.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>iDiscuss - Coding forum</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+  <?php
+  include 'partials/_header.php';
+?>
+
+<div id="carouselExampleIndicators" class="carousel slide" style="width:1350px; height:500px; margin:auto;">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner" style="height:500px;">
+    <div class="carousel-item active">
+      <img src="images/python2.img" class="d-block w-100" style="height:500px; object-fit:cover;" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/programing.img" class="d-block w-100" style="height:500px; object-fit:cover;" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="images/language.img" class="d-block w-100" style="height:500px; object-fit:cover;" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+  
+  <div class="container my-3">
+    <h2 class="text-center my-3">iDiscuss - browse Categories</h2>
+    <div class="row">
+      <!-- use a for loop to itegeate a caatgaries -->
+      <div class="col-md-4 my-2">
+        <div class="card" style="width: 18rem;">
+          <img src="images/python.img" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
+              content.</p>
+            <a href="#" class="btn btn-primary">view threads</a>
+          </div>
+        </div>
+      </div>
+            <div class="col-md-4 my-2">
+        <div class="card" style="width: 18rem;">
+          <img src="images/python.img" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
+              content.</p>
+            <a href="#" class="btn btn-primary">view threads</a>
+          </div>
+        </div>
+      </div>
+            <div class="col-md-4 my-2">
+        <div class="card" style="width: 18rem;">
+          <img src="images/python.img" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
+              content.</p>
+            <a href="#" class="btn btn-primary">view threads</a>
+          </div>
+        </div>
+      </div>
+            <div class="col-md-4 my-2">
+        <div class="card" style="width: 18rem;">
+          <img src="images/python.img" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
+              content.</p>
+            <a href="#" class="btn btn-primary">view threads</a>
+          </div>
+        </div>
+      </div>
+            <div class="col-md-4 my-2">
+        <div class="card" style="width: 18rem;">
+          <img src="images/python.img" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
+              content.</p>
+            <a href="#" class="btn btn-primary">view threads</a>
+          </div>
+        </div>
+      </div>
+            <div class="col-md-4 my-2">
+        <div class="card" style="width: 18rem;">
+          <img src="images/python.img" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
+              content.</p>
+            <a href="#" class="btn btn-primary">view threads</a>
+          </div>
+        </div>
+      </div>
+            <div class="col-md-4 my-2">
+        <div class="card" style="width: 18rem;">
+          <img src="images/python.img" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
+              content.</p>
+            <a href="#" class="btn btn-primary">view threads</a>
+          </div>
+        </div>
+      </div>
+            <div class="col-md-4 my-2">
+        <div class="card" style="width: 18rem;">
+          <img src="images/python.img" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
+              content.</p>
+            <a href="#" class="btn btn-primary">view threads</a>
+          </div>
+        </div>
+      </div>
+            <div class="col-md-4 my-2">
+        <div class="card" style="width: 18rem;">
+          <img src="images/python.img" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
+              content.</p>
+            <a href="#" class="btn btn-primary">view threads</a>
+          </div>
+        </div>
+      </div>
+      
+
+    </div>
+  </div>
+  <?php
+  include 'partials/_footer.php';
+  ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+    crossorigin="anonymous"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js"
+    integrity="sha256-S1J4GVHHDMiirir9qsXWc8ZWw74PHHafpsHp5PXtjTs=" crossorigin="anonymous"></script>
+</body>
+
+</html>
